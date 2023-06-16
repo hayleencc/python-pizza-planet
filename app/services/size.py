1f21f03eb743f84628810d2fbe59b948c111e156
Hayleen Carrillo <hayleen.carrillo@gmail.com>
2023-06-16 16:38:13 -0500

    refactor: PIZ-9 refactor services

--- app/services/size.py
+++ app/services/size.py
@@ -1,38 +1,27 @@
+from flask import Blueprint
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
-
+from app.services.BaseService import BaseService
 from ..controllers import SizeController
 
 size = Blueprint('size', __name__)
+size_service = BaseService(SizeController)
 
 
 @size.route('/', methods=POST)
 def create_size():
-    size, error = SizeController.create(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return size_service.create()
 
 
 @size.route('/', methods=PUT)
 def update_size():
-    size, error = SizeController.update(request.json)
-    response = size if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return size_service.update()
 
 
 @size.route('/', methods=GET)
 def get_all_sizes():
-    sizes, error = SizeController.get_all()
-    response = sizes if not error else {'error': error}
-    status_code = 200 if sizes else 404 if not error else 400
-    return jsonify(response), status_code
+    return size_service.get_all()
 
 
 @size.route('/id/<_id>', methods=GET)
 def get_size_by_id(_id: int):
-    size, error = SizeController.get_by_id(_id)
-    response = size if not error else {'error': error}
-    status_code = 200 if size else 404 if not error else 400
-    return jsonify(response), status_code
+    return size_service.get_by_id(_id)
